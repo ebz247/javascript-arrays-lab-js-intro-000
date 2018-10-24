46606226cdfc9a5ff9c0354427d66148749edd12
eb247 <eb247@hotmail.com>
2018-10-24 13:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var appendKitten = [...kittens, name];
+  return appendKitten;
+}
+
+function prependKitten(name) {
+ var prependKitten = [name, ...kittens]
+ return prependKitten;
+}
+
+function removeLastKitten(){
+  var removeLastKitten;
+  removeLastKitten = kittens.splice(1);
+  return removeLastKitten;
+}
+
+function removeFirstKitten(){
+  var removeFirstKitten = kittens.splice(1);
+  return removeFirstKitten;
+}
